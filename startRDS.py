9eeda5c0b0e27e9eaa60aa98906ca6e62c6dcc20
Chris Fernando <c.fernando@optusnet.au>
2021-06-01 14:56:32 +1000

    removed credentials from code

--- startRDS.py
+++ startRDS.py
@@ -1,12 +1,20 @@
 import botocore.session
+import boto3
 
+# pull RDS details from SSM parameter store
+ssm = boto3.client('ssm')
+rdsHost = ssm.get_parameter(Name='/messages/rds-instance-host', WithDecryption=False).get("Parameter").get("Value")
+rdsInstance = ssm.get_parameter(Name='/messages/rds-instance-name', WithDecryption=False).get("Parameter").get("Value")
+rdsUsername = ssm.get_parameter(Name='/messages/rds-instance-user', WithDecryption=False).get("Parameter").get("Value")
+rdsPassword = ssm.get_parameter(Name='/messages/rds-instance-pass', WithDecryption=True).get("Parameter").get("Value")
 
+# set up RDS objects
 session = botocore.session.get_session()
 rdsClient = session.create_client('rds', region_name='us-west-2')
 
 try:
 	response = rdsClient.start_db_instance(
-		DBInstanceIdentifier='jtweets'
+		DBInstanceIdentifier=rdsInstance
 	)
 	print "Start command accepted by API"
 	
@@ -23,7 +31,7 @@ except Exception, e:
 except Exception, e:
 	# set up mysql
 	try:
-		db = MySQLdb.connect("jtweets.ciizausrav91.us-west-2.rds.amazonaws.com", "jtweets", "jtweets1", "jtweets", connect_timeout=5)
+		db = MySQLdb.connect(rdsHost, rdsUsername, rdsPassword, rdsInstance, connect_timeout=5)
 	except:
 		print "Can't connect to MySQL - quitting. " + str(e)
 		exit()
--- stopRDS.py
+++ stopRDS.py
@@ -1,12 +1,20 @@
 import botocore.session
+import boto3
 
+# pull RDS details from SSM parameter store
+ssm = boto3.client('ssm')
+rdsHost = ssm.get_parameter(Name='/messages/rds-instance-host', WithDecryption=False).get("Parameter").get("Value")
+rdsInstance = ssm.get_parameter(Name='/messages/rds-instance-name', WithDecryption=False).get("Parameter").get("Value")
+rdsUsername = ssm.get_parameter(Name='/messages/rds-instance-user', WithDecryption=False).get("Parameter").get("Value")
+rdsPassword = ssm.get_parameter(Name='/messages/rds-instance-pass', WithDecryption=True).get("Parameter").get("Value")
 
+# start botocore session
 session = botocore.session.get_session()
 rdsClient = session.create_client('rds', region_name='us-west-2')	
 
 try:
 	response = rdsClient.stop_db_instance(
-		DBInstanceIdentifier='jtweets'
+		DBInstanceIdentifier=rdsInstance
 	)
 	print "Stop command accepted by API"
 	
